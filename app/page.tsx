f40a082ee2f7dd65b0a75eddbeb92e69ff53fbee
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-20 23:35:09 +0200

    feat(database): initialize database schema with User and VaultEntry tables
    - Created enum Role with values SUPERADMIN, MODERATOR, and GUEST.
    - Defined User table with fields for id, numericId, username, password, email, role, authorName, createdAt, and updatedAt.
    - Defined VaultEntry table with fields for id, numericId, title, slug, content, authorId, published, createdAt, and updatedAt.
    - Added unique indexes for numericId, username, and email in User table.
    - Added unique indexes for numericId and slug in VaultEntry table.
    - Established foreign key relationship between VaultEntry and User tables.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+import prisma from '@/lib/prisma';
+
+export default async function Home() {
+  const publishedPosts = await prisma.vaultEntry.findMany({
+    where: { published: true },
+  });
+  console.log(publishedPosts);
   return (
     <>
       <h1>This is currently a Work in progress. Stay tuned</h1>
