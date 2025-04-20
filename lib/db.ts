4400c12a8ad5ad5d053ad746fcf243da05263ceb
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-21 01:32:36 +0200

    feat: implement fetchEntriesMetadata function to retrieve published vault entries
    Added a new function to fetch metadata for published vault entries using Prisma. This enhances data retrieval capabilities and improves overall application functionality.

---
+++ lib/db.ts
@@ -0,0 +1,24 @@
+import { cache } from 'react';
+import prisma from './prisma';
+
+const fetchEntriesMetadata = cache(
+  async (): Promise<
+    Array<{
+      slug: string;
+      title: string;
+      category: string;
+    }>
+  > => {
+    return prisma.vaultEntry.findMany({
+      select: { slug: true, title: true, category: true },
+      where: { published: true },
+    });
+  },
+);
+const fetchEntryBySlug = async () => {};
+// const getEntryBySlug = async () => {};
+// const getEntryBySlug = async () => {};
+// const getEntryBySlug = async () => {};
+// const getEntryBySlug = async () => {};
+
+export { fetchEntryBySlug, fetchEntriesMetadata };
