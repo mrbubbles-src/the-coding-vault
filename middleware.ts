203a86b761ef95cae0fff712d5089943c5061fc7
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-24 00:29:13 +0200

    feat(middleware): enhance role-based access control for admin routes
    Refactor middleware to improve role verification for admin dashboard access.
    Redirects users based on their roles: SUPERADMIN can access directly,
    while MODERATOR and GUEST are redirected to their respective dashboards.
    Also, ensure that the login route is accessible without restrictions.

--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,28 @@ import { verifyRole } from './lib/auth';
 
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
+
+  if (pathname === '/admin/login') {
+    return NextResponse.next();
+  }
+
   const isSuperAdmin = await verifyRole('SUPERADMIN');
   const isModerator = await verifyRole('MODERATOR');
   const isGuest = await verifyRole('GUEST');
 
   if (isSuperAdmin) return NextResponse.next();
 
-  if (pathname.startsWith('/moderator') && isModerator)
-    return NextResponse.next();
+  if (pathname === '/admin/dashboard' && isModerator) {
+    return NextResponse.redirect(new URL('/admin/mod/dashboard', req.url));
+  }
 
-  if ((pathname.startsWith('/guest') && isGuest) || isModerator)
-    return NextResponse.next();
+  if (pathname === '/admin/dashboard' && isGuest) {
+    return NextResponse.redirect(new URL('/admin/guest/dashboard', req.url));
+  }
 
-  return NextResponse.redirect(new URL('/', req.url));
+  return NextResponse.redirect(new URL('/admin/login', req.url));
 }
+
+export const config = {
+  matcher: ['/admin/:path*'],
+};
