ea37876fb1156a43863ebd29cfb1cb42483de64b
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-23 22:57:55 +0200

    feat(auth): add verifyRole function to check user roles from JWT token
    Implement the verifyRole function that retrieves the JWT token from cookies, verifies it, and checks if the user's role matches the required role. This enhances role-based access control in the application.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken';
+import { getCookie } from './cookies';
+import { TJWT, TRole } from './types';
 
 const secret = process.env.JWT_SECRET as string;
 
@@ -10,4 +12,16 @@ const verifyJWT = (token: string): string | jwt.JwtPayload => {
   return jwt.verify(token, secret);
 };
 
-export { createJWT, verifyJWT };
+const verifyRole = async (requiredRole: TRole): Promise<boolean> => {
+  try {
+    const token = await getCookie('token');
+    if (!token) return false;
+    const decodedToken = verifyJWT(token) as TJWT;
+    return decodedToken.role === requiredRole;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+export { createJWT, verifyJWT, verifyRole };
