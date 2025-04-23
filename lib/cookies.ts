e8ac61ba7539e091a1af5e891fa6b17a3c1baeda
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-23 02:34:34 +0200

    feat(cookies): implement createCookie function for token and theme management

---
+++ lib/cookies.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+const createCookie = async (token?: string, theme?: string): Promise<void> => {
+  const cookie = await cookies();
+
+  if (token) {
+    cookie.set('token', token, {
+      httpOnly: true,
+      secure: true,
+      path: '/',
+      maxAge: 60 * 60 * 24 * 30,
+      sameSite: 'lax',
+    });
+  }
+
+  if (theme) {
+    cookie.set('theme', theme, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 365,
+      sameSite: 'lax',
+    });
+  }
+};
+
+export { createCookie };
