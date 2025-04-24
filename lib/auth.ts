22a8a716c615d948216ffbd6b924ecb36d250447
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-24 03:33:39 +0200

    refactor(auth): replace jsonwebtoken with jose for improved JWT handling and security
    - Updated JWT creation and verification to use the 'jose' library.
    - Enhanced type safety by using IJWT and JWTPayload interfaces.
    - Converted createJWT and verifyJWT functions to async for better handling of promises.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,22 +1,29 @@
-import jwt from 'jsonwebtoken';
+import { JWTPayload, SignJWT, jwtVerify } from 'jose';
 import { getCookie } from './cookies';
-import { TJWT, TRole } from './types';
+import { IJWT, TRole } from './types';
 
-const secret = process.env.JWT_SECRET as string;
-
-const createJWT = (payload: object): string => {
-  return jwt.sign(payload, secret, { expiresIn: '30d' });
+const createJWT = async (payload: IJWT & JWTPayload): Promise<string> => {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setExpirationTime('30d')
+    .sign(secret);
 };
 
-const verifyJWT = (token: string): string | jwt.JwtPayload => {
-  return jwt.verify(token, secret);
+const verifyJWT = async (token: string): Promise<IJWT & JWTPayload> => {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const { payload } = await jwtVerify(token, secret);
+  if (typeof payload.id === 'string' && typeof payload.role === 'string') {
+    return payload as IJWT & JWTPayload;
+  }
+  throw new Error('Invalid JWT payload');
 };
 
 const verifyRole = async (requiredRole: TRole): Promise<boolean> => {
   try {
     const token = await getCookie('token');
     if (!token) return false;
-    const decodedToken = verifyJWT(token) as TJWT;
+    const decodedToken = (await verifyJWT(token)) as IJWT;
     return decodedToken.role === requiredRole;
   } catch (error) {
     console.error(error);
