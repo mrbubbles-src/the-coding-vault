b5492c6bc04f15236eb2f2d1378ac7fcb10c594c
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-24 03:33:55 +0200

    feat(spinner): add loading spinner component for UI feedback

---
+++ components/ui/loading/spinner.tsx
@@ -0,0 +1,7 @@
+const Spinner = () => {
+  return (
+    <div className="h-8 w-8 animate-spin rounded-full border-4 border-dotted border-red-500 border-t-transparent" />
+  );
+};
+
+export default Spinner;
