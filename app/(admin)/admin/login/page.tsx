5c0d8c5d7980948fe7c938fcb688be217ba472c5
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-24 03:34:40 +0200

    feat(admin-login): implement AdminLoginPage with LoginForm component

--- app/(admin)/admin/login/page.tsx
+++ app/(admin)/admin/login/page.tsx
@@ -1,5 +1,13 @@
+import LoginForm from '@/components/auth/login-form';
+
 const AdminLoginPage = () => {
-  return <>AdminLoginPage</>;
+  return (
+    <section>
+      <h1>Login</h1>
+      {/* TODO: Stylen der LoginForm/LoginPage */}
+      <LoginForm />
+    </section>
+  );
 };
 
 export default AdminLoginPage;
