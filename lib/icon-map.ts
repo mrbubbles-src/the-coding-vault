cf0ba7193b25315b2295dabc30737457d8c98f4c
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-25 02:12:47 +0200

    feat(icon-map): create iconMap for standardized icon usage with TIconKey type

---
+++ lib/icon-map.ts
@@ -0,0 +1,27 @@
+import {
+  faHtml5,
+  faReact,
+  faNodeJs,
+  faGithub,
+  faCss3Alt,
+  faJs,
+  IconDefinition,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faDatabase,
+  faQuestionCircle,
+  faServer,
+} from '@fortawesome/free-solid-svg-icons';
+import { TIconKey } from './types';
+
+export const iconMap: Record<TIconKey, IconDefinition> = {
+  github: faGithub,
+  node: faNodeJs,
+  html: faHtml5,
+  css: faCss3Alt,
+  js: faJs,
+  react: faReact,
+  backend: faServer,
+  database: faDatabase,
+  default: faQuestionCircle,
+};
