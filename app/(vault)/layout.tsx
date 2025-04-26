217b05359401254ef564882f766fe65be6ff3e7c
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-27 00:45:59 +0200

    refactor(layout): replace Geist fonts with Fira fonts and update sidebar structure
    - Removed Geist font imports and replaced them with Fira fonts for improved typography.
    - Updated the layout to use VaultSidebar and Navbar components, enhancing the sidebar structure and layout consistency.

--- app/(vault)/layout.tsx
+++ app/(vault)/layout.tsx
@@ -1,22 +1,31 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import '../globals.css';
-import {
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/shadcn/sidebar';
-import AppSidebar from '@/components/ui/sidebar/vault/app-sidebar';
 import { cookies } from 'next/headers';
+import { Fira_Code, Fira_Mono, Montserrat, Poppins } from 'next/font/google';
+import { SidebarProvider } from '@/components/ui/shadcn/sidebar';
 import { ThemeProvider } from '@/context/theme-provider';
+import VaultSidebar from '@/components/ui/sidebar/vault/vault-sidebar';
+import Navbar from '@/components/ui/navigation/navbar';
+import '../globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const firaCode = Fira_Code({
+  variable: '--font-fira-code',
   subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
 });
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const firaCodeMono = Fira_Mono({
+  variable: '--font-fira-code-mono',
   subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
+const montserrat = Montserrat({
+  variable: '--font-montserrat',
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+});
+const poppins = Poppins({
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
@@ -34,18 +43,18 @@ export default async function RootLayout({
   return (
     <html lang="de" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${firaCode.variable} ${firaCodeMono.variable} ${montserrat.variable} ${poppins.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange>
           <SidebarProvider defaultOpen={defaultOpen}>
-            <AppSidebar />
-            <main>
-              <SidebarTrigger />
-              {children}
-            </main>
+            <VaultSidebar />
+            <div className="w-full py-2 pr-2 pl-1">
+              <Navbar />
+              <main className="py-2 pr-2 pl-1">{children}</main>
+            </div>
           </SidebarProvider>
         </ThemeProvider>
       </body>
