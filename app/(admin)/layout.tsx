0f67a36e9a418f2926287d5c3fddfd18d75945f7
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-27 00:41:32 +0200

    refactor(layout): replace Geist fonts with Fira fonts and integrate ThemeProvider
    Updated the layout to use Fira_Code, Fira_Mono, Montserrat, and Poppins fonts instead of Geist fonts. Integrated ThemeProvider for improved theme management and ensured proper font application across the layout.

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,15 +1,28 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Fira_Code, Fira_Mono, Montserrat, Poppins } from 'next/font/google';
 import '../globals.css';
+import { ThemeProvider } from '@/context/theme-provider';
+import Navbar from '@/components/ui/navigation/navbar';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const firaCode = Fira_Code({
+  variable: '--font-fira-code',
   subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
 });
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const firaCodeMono = Fira_Mono({
+  variable: '--font-fira-code-mono',
   subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
+const montserrat = Montserrat({
+  variable: '--font-montserrat',
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+});
+const poppins = Poppins({
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
@@ -23,10 +36,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="de" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <main>{children}</main>
+        className={`${firaCode.variable} ${firaCodeMono.variable} ${montserrat.variable} ${poppins.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <Navbar />
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
