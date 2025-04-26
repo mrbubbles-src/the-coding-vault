f00a93e60d38d431bcd1f5eba49095b18419f44f
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-27 01:52:42 +0200

    fix(spinner): update border color to use secondary color for consistency

--- components/ui/loading/spinner.tsx
+++ components/ui/loading/spinner.tsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
   return (
-    <div className="h-8 w-8 animate-spin rounded-full border-4 border-dotted border-red-500 border-t-transparent" />
+    <div className="border-secondary h-8 w-8 animate-spin rounded-full border-4 border-dotted border-t-transparent" />
   );
 };
 
