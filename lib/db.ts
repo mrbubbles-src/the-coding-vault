d36131921022401a631a7b1f7a45be2f840f7cf0
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-27 19:55:28 +0200

    refactor(db): enhance getCategories function with error handling
    Refactor the getCategories function to include error handling. The function now catches errors during the database query and logs an error message, returning an empty array in case of failure.

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,16 @@
 import prisma from './prisma';
 import { ICategories } from './types';
 
-const getCategories = async (): Promise<Array<ICategories>> =>
-  await prisma.category.findMany({
-    orderBy: { order: 'asc' },
-  });
+const getCategories = async (): Promise<Array<ICategories>> => {
+  try {
+    return await prisma.category.findMany({
+      orderBy: { order: 'asc' },
+    });
+  } catch (error) {
+    console.error('Fehler beim Abrufen der Kategorien:', error);
+    return [];
+  }
+};
 // const getEntryBySlug = async () => {};
 // const fetchEntryBySlug = async () => {};
 // // const getEntryBySlug = async () => {};
