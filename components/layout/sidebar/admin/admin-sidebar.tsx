585927d4b69507a3e501db44c7b456810c9a0421
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-27 22:31:26 +0200

    feat(admin-sidebar): enhance user management features and add entry deletion capability
    - Implement user role verification to conditionally display user management options.
    - Add "Delete Entry" option for users with appropriate permissions.
    - Update sidebar to reflect logged-in user's username.

--- components/layout/sidebar/admin/admin-sidebar.tsx
+++ components/layout/sidebar/admin/admin-sidebar.tsx
@@ -35,10 +35,15 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from '@/components/ui/shadcn/collapsible';
-import { handleLogout } from '@/lib/utils';
 import { LogoutButton } from '@/components/ui/user-logout';
+import { getCookie } from '@/lib/cookies';
+import { verifyJWT } from '@/lib/auth';
+import { canDeleteEntry, canManageUsers } from '@/lib/roles';
 
 const AdminSidebar = async () => {
+  const token = await getCookie('token');
+  if (!token) return null;
+  const loggedInUser = await verifyJWT(token);
   return (
     <Sidebar collapsible="icon" variant="floating">
       {/* ? Header Start */}
@@ -109,6 +114,19 @@ const AdminSidebar = async () => {
                   </SidebarMenuSubItem>
                 </SidebarMenuSub>
               </SidebarGroupContent>
+              {loggedInUser && canDeleteEntry(loggedInUser.role) && (
+                <SidebarGroupContent>
+                  <SidebarMenuSub>
+                    <SidebarMenuSubItem>
+                      <SidebarMenuButton>
+                        <Link href={`/`}>
+                          <span>Delete Entry</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuSubItem>
+                  </SidebarMenuSub>
+                </SidebarGroupContent>
+              )}
             </CollapsibleContent>
           </SidebarGroup>
         </Collapsible>
@@ -142,55 +160,61 @@ const AdminSidebar = async () => {
         </Collapsible>
         {/* ? Group 2 End */}
         {/* ? Group 3 Start */}
-        <Collapsible className="group/collapsible">
-          <SidebarGroup>
-            <div className="flex items-center gap-2">
-              <Users className="h-5 w-5 shrink-0" />
-              <SidebarGroupLabel asChild>
-                <CollapsibleTrigger className="flex flex-1 items-center gap-2">
-                  <span className="text-lg font-semibold">User Management</span>
-                  <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
-                </CollapsibleTrigger>
-              </SidebarGroupLabel>
-            </div>
-            <CollapsibleContent>
-              <SidebarGroupContent>
-                <SidebarMenuSub>
-                  <SidebarMenuSubItem>
-                    <SidebarMenuButton>
-                      <Link href={``}>
-                        <span>All Users</span>
-                      </Link>
-                    </SidebarMenuButton>
-                    <SidebarMenuBadge className="text-sm">(3)</SidebarMenuBadge>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-              </SidebarGroupContent>
-              <SidebarGroupContent>
-                <SidebarMenuSub>
-                  <SidebarMenuSubItem>
-                    <SidebarMenuButton>
-                      <Link href={``}>
-                        <span>Create New User</span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-              </SidebarGroupContent>
-              <SidebarGroupContent>
-                <SidebarMenuSub>
-                  <SidebarMenuSubItem>
-                    <SidebarMenuButton>
-                      <Link href={``}>
-                        <span>Delete User</span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuSubItem>
-                </SidebarMenuSub>
-              </SidebarGroupContent>
-            </CollapsibleContent>
-          </SidebarGroup>
-        </Collapsible>
+        {loggedInUser && canManageUsers(loggedInUser.role) && (
+          <Collapsible className="group/collapsible">
+            <SidebarGroup>
+              <div className="flex items-center gap-2">
+                <Users className="h-5 w-5 shrink-0" />
+                <SidebarGroupLabel asChild>
+                  <CollapsibleTrigger className="flex flex-1 items-center gap-2">
+                    <span className="text-lg font-semibold">
+                      User Management
+                    </span>
+                    <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                  </CollapsibleTrigger>
+                </SidebarGroupLabel>
+              </div>
+              <CollapsibleContent>
+                <SidebarGroupContent>
+                  <SidebarMenuSub>
+                    <SidebarMenuSubItem>
+                      <SidebarMenuButton>
+                        <Link href={``}>
+                          <span>All Users</span>
+                        </Link>
+                      </SidebarMenuButton>
+                      <SidebarMenuBadge className="text-sm">
+                        (3)
+                      </SidebarMenuBadge>
+                    </SidebarMenuSubItem>
+                  </SidebarMenuSub>
+                </SidebarGroupContent>
+                <SidebarGroupContent>
+                  <SidebarMenuSub>
+                    <SidebarMenuSubItem>
+                      <SidebarMenuButton>
+                        <Link href={``}>
+                          <span>Create New User</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuSubItem>
+                  </SidebarMenuSub>
+                </SidebarGroupContent>
+                <SidebarGroupContent>
+                  <SidebarMenuSub>
+                    <SidebarMenuSubItem>
+                      <SidebarMenuButton>
+                        <Link href={``}>
+                          <span>Delete User</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuSubItem>
+                  </SidebarMenuSub>
+                </SidebarGroupContent>
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+        )}
         {/* ? Group 3 End */}
       </SidebarContent>
       {/* ? Content End */}
@@ -202,12 +226,15 @@ const AdminSidebar = async () => {
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <SidebarMenuButton className="group">
-                  <User2 /> Admin{' '}
+                  <User2 /> {loggedInUser.username}
                   <ChevronUp className="ml-auto transition-transform group-data-[state=open]:rotate-180" />
                 </SidebarMenuButton>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end">
                 <DropdownMenuItem>
+                  <Link href={'/'}>Vault</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem variant="destructive">
                   <LogoutButton />
                 </DropdownMenuItem>
               </DropdownMenuContent>
