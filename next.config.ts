ff55a3bdf26d030f3babb2af40f19295e3dc5f5a
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-01 00:00:31 +0200

    refactor(next.config.ts): update configuration for improved MDX support and experimental features

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,38 @@
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
+// import remarkGfm from 'remark-gfm';
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
 
 const nextConfig: NextConfig = {
   /* config options here */
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  reactStrictMode: true,
+  poweredByHeader: false,
   serverExternalPackages: ['@prisma/client'],
   experimental: {
+    typedRoutes: true,
     useCache: true,
+    mdxRs: {
+      mdxType: 'gfm',
+    },
   },
 };
+
+// /** @type {import('remark-gfm').Options} */
+// const remarkGfmOptions = {
+//   singleTilde: false,
+// };
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
+  extension: /\.mdx?$/,
+  // options: {
+  //   remarkPlugins: [[remarkGfm, remarkGfmOptions]],
+  //   rehypePlugins: [],
+  // },
 });
 export default withMDX(nextConfig);
 
