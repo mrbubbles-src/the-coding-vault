b7511c5e1d49c334bfd96031c72347d9558bd228
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-30 21:01:36 +0200

    feat(home): restructure Home component layout and integrate logo image

--- app/(vault)/page.tsx
+++ app/(vault)/page.tsx
@@ -1,7 +1,15 @@
+import HomeLogo from '@/public/images/homelogo.svg';
+import Image from 'next/image';
 export default function Home() {
   return (
-    <>
+    <header className="flex flex-col items-center justify-center">
       <h1>This is currently a Work in progress. Stay tuned</h1>
-    </>
+      <Image
+        src={HomeLogo}
+        alt='The Coding Vault Logo With Macot "Vaulty"'
+        width={600}
+        height={600}
+      />
+    </header>
   );
 }
