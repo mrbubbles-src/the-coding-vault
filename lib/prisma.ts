28503b0fc9cade39b157c5c8c398d99709ebf0fb
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-01 02:02:36 +0200

    feat(prisma): integrate Prisma Accelerate extension for improved performance
    - Updated Prisma client initialization to use the withAccelerate extension.
    - Updated package dependencies for @prisma/client and added @prisma/extension-accelerate.
    - Modified build and postinstall scripts to use --no-engine flag for Prisma generation.
    - Adjusted Prisma schema generator configuration to remove engineType for compatibility.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,6 @@
 // import { PrismaClient } from '@prisma/client';
 import { PrismaClient } from '@/generated/prisma/client'; // falls das mit dem oberen nicht funzt
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -16,7 +17,8 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
  *
  * @see {@link https://www.prisma.io/docs/concepts/components/prisma-client Prisma Client Documentation}
  */
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
