5f742b7736a5d3052ce0f22c9a00f183d3eb03c7
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-02 21:54:28 +0200

    refactor(middleware): simplify authentication flow and role verification
    - Remove redundant role checks for user redirection.
    - Streamline the middleware logic to focus on token validation and user redirection based on authentication status.
    - Ensure users are redirected to the appropriate dashboard based on their login state.

--- middleware.ts
+++ middleware.ts
@@ -1,49 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { verifyJWT, verifyRole } from './lib/auth';
+import { verifyJWT } from './lib/auth';
 import { getCookie } from './lib/cookies';
 
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
-  if (pathname === '/admin/login') {
-    const token = await getCookie('token');
+  const token = await getCookie('token');
+  if (!token) return NextResponse.redirect(new URL('/admin/login', req.url));
 
-    if (token) {
-      const user = await verifyJWT(token);
-
-      if (user.role === 'SUPERADMIN') {
-        return NextResponse.redirect(new URL('/admin/dashboard', req.url));
-      }
-
-      if (user.role === 'MODERATOR') {
-        return NextResponse.redirect(new URL('/admin/mod/dashboard', req.url));
-      }
-
-      if (user.role === 'GUEST') {
-        return NextResponse.redirect(
-          new URL('/admin/guest/dashboard', req.url),
-        );
-      }
-    }
-
-    return NextResponse.next();
+  let user;
+  try {
+    user = await verifyJWT(token);
+  } catch {
+    return NextResponse.redirect(new URL('/admin/login', req.url));
   }
 
-  const isSuperAdmin = await verifyRole('SUPERADMIN');
-  const isModerator = await verifyRole('MODERATOR');
-  const isGuest = await verifyRole('GUEST');
-
-  if (isSuperAdmin) return NextResponse.next();
-
-  if (pathname === '/admin/dashboard' && isModerator) {
-    return NextResponse.redirect(new URL('/admin/mod/dashboard', req.url));
-  }
-
-  if (pathname === '/admin/dashboard' && isGuest) {
-    return NextResponse.redirect(new URL('/admin/guest/dashboard', req.url));
-  }
-
-  return NextResponse.redirect(new URL('/admin/login', req.url));
+  if (pathname.startsWith('/admin/login') && user)
+    return NextResponse.redirect(new URL('/admin/dashboard', req.url));
+  return NextResponse.next();
 }
 
 export const config = {
