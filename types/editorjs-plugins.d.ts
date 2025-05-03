b36c0b96cf4a2bd0a121120c6eebec6dace4e521
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-03 19:16:51 +0200

    feat(types): add TypeScript definitions for Editor.js plugins

---
+++ types/editorjs-plugins.d.ts
@@ -0,0 +1,58 @@
+declare module '@editorjs/embed' {
+  import { ToolConstructable } from '@editorjs/editorjs';
+  const Embed: ToolConstructable;
+  export default Embed;
+}
+declare module '@editorjs/inline-code' {
+  import { InlineTool } from '@editorjs/editorjs';
+  const InlineCode: InlineTool;
+  export default InlineCode;
+}
+
+declare module '@editorjs/attaches' {
+  import { BlockTool } from '@editorjs/editorjs';
+  const AttachesTool: BlockTool;
+  export default AttachesTool;
+}
+
+declare module '@bomdi/codebox' {
+  import { BlockTool } from '@editorjs/editorjs';
+  const CodeBox: BlockTool;
+  export default CodeBox;
+}
+
+declare module '@coolbytes/editorjs-delimiter' {
+  import { BlockTool } from '@editorjs/editorjs';
+  const Delimiter: BlockTool;
+  export default Delimiter;
+}
+
+declare module 'editorjs-toggle-block' {
+  import { BlockTool } from '@editorjs/editorjs';
+  const ToggleBlock: BlockTool;
+  export default ToggleBlock;
+}
+
+declare module 'editorjs-alert' {
+  import { BlockTool } from '@editorjs/editorjs';
+  const Alert: BlockTool;
+  export default Alert;
+}
+
+declare module '@sotaproject/strikethrough' {
+  import { InlineTool } from '@editorjs/editorjs';
+  const Strikethrough: InlineTool;
+  export default Strikethrough;
+}
+
+declare module 'editorjs-annotation' {
+  import { InlineTool } from '@editorjs/editorjs';
+  const Annotation: InlineTool;
+  export default Annotation;
+}
+
+declare module 'editorjs-inline-hotkey' {
+  import { InlineTool } from '@editorjs/editorjs';
+  const EditorJSInlineHotkey: InlineTool;
+  export default EditorJSInlineHotkey;
+}
