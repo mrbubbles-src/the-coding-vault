a651614c3c3d23edeec18c33ed8fbe134b3f5cce
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-05 01:33:05 +0200

    feat(details-toggle): add DetailsToggle component for collapsible content display

---
+++ components/layout/vault/details-toggle.tsx
@@ -0,0 +1,31 @@
+import { Card, CardContent } from '@/components/ui/shadcn/card';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/shadcn/collapsible';
+import { ChevronRight } from 'lucide-react';
+
+const DetailsToggle = ({
+  text,
+  children,
+}: {
+  text?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Collapsible className="group/collapsible">
+      <CollapsibleTrigger className="group-data-[state=open]/collapsible:text-primary flex cursor-pointer place-items-center text-lg font-semibold transition-colors">
+        <ChevronRight className="transition-transform duration-500 ease-in-out group-data-[state=open]/collapsible:rotate-90" />
+        {text}
+      </CollapsibleTrigger>
+      <CollapsibleContent className="group-data-[state=open]/collapsible:animate-collapsible-down animate-collapsible-up transition-all duration-500 ease-in-out">
+        <Card className="opacity-0 transition-all duration-500 ease-in-out group-data-[state=open]/collapsible:opacity-100">
+          <CardContent className="text-lg">{children}</CardContent>
+        </Card>
+      </CollapsibleContent>
+    </Collapsible>
+  );
+};
+
+export default DetailsToggle;
