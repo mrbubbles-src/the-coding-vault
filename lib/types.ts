38e6bc235b6d6f07ddf9c13a5efa5675d2943fc3
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-16 02:09:59 +0200

    feat(types): add TypeScript types and interfaces for user roles, JWT, inputs, vault entries, icons, and categories

---
+++ lib/types.ts
@@ -0,0 +1,47 @@
+export type TRole = 'SUPERADMIN' | 'MODERATOR' | 'GUEST';
+
+export interface IJWT {
+  id: string;
+  username: string;
+  role: TRole;
+}
+
+export interface IInputs {
+  username: string;
+  password: string;
+}
+
+export interface IVaultEntry {
+  id: string;
+  numericId: number;
+  title: string;
+  slug: string;
+  content: string;
+  categoryId: string;
+  category: string;
+  authorId: string;
+  author: string;
+  published: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export type TIconKey =
+  | 'git'
+  | 'github'
+  | 'node'
+  | 'html'
+  | 'css'
+  | 'js'
+  | 'react'
+  | 'backend'
+  | 'database'
+  | 'default';
+
+export interface ICategories {
+  id: string;
+  name: string;
+  slug: string;
+  iconKey: TIconKey;
+  order: number;
+  entries?: Array<IVaultEntry>;
+}
