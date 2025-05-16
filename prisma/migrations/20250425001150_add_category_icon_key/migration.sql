b98284b757a916a5232eb8f1066dca6f8cb97acd
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-16 02:09:04 +0200

    refactor(prisma): remove obsolete migration files and schema definitions

--- prisma/migrations/20250425001150_add_category_icon_key/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "IconKey" AS ENUM ('github', 'node', 'html', 'css', 'js', 'react', 'backend', 'database', 'default');
-
--- AlterTable
-ALTER TABLE "Category" ADD COLUMN     "iconKey" "IconKey" NOT NULL DEFAULT 'default';
