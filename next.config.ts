48d16bb258e3ee24f7702f579ed48d1f59ecd8d9
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-16 02:09:31 +0200

    refactor(next.config): clean up configuration by removing commented code and unnecessary options

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
-// import remarkGfm from 'remark-gfm';
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
   reactStrictMode: true,
   poweredByHeader: false,
-  serverExternalPackages: ['@prisma/client'],
   experimental: {
     typedRoutes: true,
     useCache: true,
@@ -22,18 +15,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-// /** @type {import('remark-gfm').Options} */
-// const remarkGfmOptions = {
-//   singleTilde: false,
-// };
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   extension: /\.mdx?$/,
-  // options: {
-  //   remarkPlugins: [[remarkGfm, remarkGfmOptions]],
-  //   rehypePlugins: [],
-  // },
 });
 export default withMDX(nextConfig);
-
-// export default nextConfig;
