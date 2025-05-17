90f8a8d00e9b227641ca31c31384a47670aaf958
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-17 23:58:30 +0200

    fix(layout): update default sidebar state to closed based on cookie value

--- app/(vault)/layout.tsx
+++ app/(vault)/layout.tsx
@@ -60,7 +60,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const cookieStore = await cookies();
-  const defaultOpen = cookieStore.get('sidebar_state')?.value === 'true';
+  const defaultOpen = cookieStore.get('sidebar_state')?.value === 'false';
   return (
     <html lang="de" suppressHydrationWarning>
       <body
