442d03b8953c80953512aefe612eb39a8b46a7f6
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-17 22:51:11 +0200

    refactor(db): update fetch URL in getCategories function to use base URL from environment variable

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,12 @@
 // import prisma from './prisma';
-import { Route } from 'next';
+// import { Route } from 'next';
 import { ICategories } from '@/types/types';
 
 const getCategories = async (): Promise<Array<ICategories>> => {
   try {
     const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
     console.log('Fetch baseUrl:', baseUrl);
-    const res = await fetch(`${baseUrl}/api/vault/categories` as Route, {
+    const res = await fetch(`/api/vault/categories`, {
       next: { revalidate: 86400 },
     });
     const data: Array<ICategories> = await res.json();
