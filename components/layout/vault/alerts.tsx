a43c9140b59520faf7c4d90c3bfd00e3699869cf
mrbubbles-src <manuel_fahrenholz@web.de>
2025-05-18 02:33:07 +0200

    refactor(alerts): enhance alert component structure and styling
    - Added CardFooter to include branding with an image.
    - Improved alert styles by adding shadow effects for better visibility.
    - Updated alert titles to include icons with consistent sizing.
    - Adjusted text styles for better readability and layout consistency.

--- components/layout/vault/alerts.tsx
+++ components/layout/vault/alerts.tsx
@@ -1,10 +1,14 @@
 import {
   Card,
   CardContent,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/shadcn/card';
 import { CircleAlert, Skull, Smile, TriangleAlert } from 'lucide-react';
+import Vaulty from '@/public/images/icon.svg';
+
+import Image from 'next/image';
 const Alerts = ({
   type = 'info',
   message,
@@ -14,41 +18,60 @@ const Alerts = ({
   message?: string;
 }) => {
   const alertStyles = {
-    info: 'bg-blue-100 border-blue-300 border-2 text-blue-600',
-    success: 'bg-green-100 border-green-300 border-2 text-green-600',
-    warning: 'bg-orange-100 border-orange-300 border-2 text-orange-600',
-    danger: 'bg-red-100 border-red-300 border-2 text-red-600',
+    info: 'bg-blue-100 shadow-md border-blue-300 border-2 text-blue-600',
+    success: 'bg-green-100 shadow-md border-green-300 border-2 text-green-600',
+    warning:
+      'bg-orange-100 shadow-md border-orange-300 border-2 text-orange-600',
+    danger: 'bg-red-100 shadow-md border-red-300 border-2 text-red-600',
   };
   const alertTitles = {
     info: (
       <>
-        <CircleAlert /> Info!
+        <CircleAlert className="size-6" />
+        <span>Wusstest Du schon?</span>
       </>
     ),
     success: (
       <>
-        <Smile /> Success!
+        <Smile className="size-6" />
+        <span>Ah, der Klassiker!</span>
       </>
     ),
     warning: (
       <>
-        <TriangleAlert /> Warnung!
+        <TriangleAlert className="size-6" />
+        <span>Vorsicht ist besser als Nachsicht!</span>
       </>
     ),
     danger: (
       <>
-        <Skull /> Danger!
+        <Skull className="size-6" />
+        <span>Welcome, to the DANGER ZONE!</span>
       </>
     ),
   };
   return (
-    <Card className={`${alertStyles[type]}`}>
+    <Card className={`${alertStyles[type]} my-4 text-pretty`}>
       <CardHeader>
-        <CardTitle className="flex place-items-center gap-1 text-2xl">
-          {alertTitles[type]}
+        <CardTitle className="flex place-items-center gap-1 text-xl font-bold">
+          <h3 className="flex place-items-center gap-1">{alertTitles[type]}</h3>
         </CardTitle>
       </CardHeader>
-      <CardContent className="ml-[0.15rem] text-xl">{message}</CardContent>
+      <CardContent className="ml-[0.15rem] text-lg font-semibold">
+        {message}
+      </CardContent>
+      <CardFooter className="justify-end pr-4">
+        <span className="flex place-items-center gap-1 text-sm font-semibold italic">
+          – Vaulty{' '}
+          <Image
+            src={Vaulty}
+            alt="Vaulty"
+            width={40}
+            height={40}
+            className="transition-transform duration-300 ease-in-out hover:scale-110"
+          />
+        </span>
+      </CardFooter>
     </Card>
   );
 };
