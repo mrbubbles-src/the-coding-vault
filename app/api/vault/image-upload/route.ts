cdbd5f8b241119f8add3cfece90cbb225911bde4
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-05-19 00:57:50 +0200

    feat(image-upload): implement image upload functionality using Cloudinary API
    Add a new route for handling image uploads. This includes configuration for Cloudinary and a POST method that processes form data, uploads the image, and returns the upload result in JSON format. This feature enhances the application by allowing users to upload images to a cloud storage service.

---
+++ app/api/vault/image-upload/route.ts
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { v2 as cloudinary } from 'cloudinary';
+import type { UploadApiResponse } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export async function POST(req: NextRequest) {
+  const formData = await req.formData();
+  console.log('formData', formData);
+  const file = formData.get('image') as File;
+
+  if (!file) {
+    return NextResponse.json(
+      { success: 0, message: 'No file uploaded' },
+      { status: 400 },
+    );
+  }
+
+  const buffer = Buffer.from(await file.arrayBuffer());
+
+  const uploadResult = await new Promise<UploadApiResponse>(
+    (resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          {
+            folder: 'vault-uploads',
+            filename_override: file.name.split('.')[0],
+          },
+          (error, result) => {
+            if (error || !result) reject(error);
+            else resolve(result);
+          },
+        )
+        .end(buffer);
+    },
+  );
+  console.log('uploadResult', uploadResult);
+  const { secure_url, display_name, original_filename, width, height } =
+    uploadResult;
+
+  return NextResponse.json({
+    success: 1,
+    file: {
+      url: secure_url,
+      original_filename: original_filename,
+      display_name: display_name,
+      width: width,
+      height: height,
+    },
+  } satisfies {
+    success: 1;
+    file: {
+      url: string;
+      original_filename: string;
+      display_name: string;
+      width: number;
+      height: number;
+    };
+  });
+}
