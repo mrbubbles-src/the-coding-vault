5119b48141d773ca9ee628ef47cd90c429c7ce80
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-05-19 03:02:05 +0200

    feat(vault-image): implement VaultImage component using CldImage for rendering images
    - Introduced the VaultImage component that utilizes the newly created CldImage component for displaying images.
    - The VaultImage component fetches image data from Cloudinary and handles base64 conversion for optimized loading.
    - This change enhances the image rendering capabilities within the application.

---
+++ components/layout/vault/vault-image.tsx
@@ -0,0 +1,51 @@
+import CldImage from './cdimage';
+import { getCldImageUrl } from 'next-cloudinary';
+
+const VaultImage = async ({
+  url,
+  caption,
+  original_filename,
+  public_id,
+  width,
+  height,
+}: {
+  url: string;
+  caption: string;
+  original_filename: string;
+  public_id: string;
+  width: number;
+  height: number;
+}) => {
+  const imageUrl = getCldImageUrl({
+    src: public_id,
+  });
+  const response = await fetch(imageUrl);
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  const base64 = buffer.toString('base64');
+  const dataUrl = `data:${response.type};base64,${base64}`;
+
+  return (
+    <figure className="text-center">
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <CldImage
+          src={'vault-uploads/ulfbk8s3cp9jreqxsxqb'}
+          alt={caption || original_filename}
+          width={width}
+          height={height}
+          sizes="100vw"
+          placeholder="blur"
+          blurDataURL={dataUrl}
+          className="rounded-md shadow-md"
+        />
+      </a>
+      {caption && (
+        <figcaption className="mt-1 text-sm text-gray-600">
+          {caption}
+        </figcaption>
+      )}
+    </figure>
+  );
+};
+
+export default VaultImage;
