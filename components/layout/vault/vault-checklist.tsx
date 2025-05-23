81264a853d4315a20bb06f7fa44680d51cc90518
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-05-23 23:51:24 +0200

    feat(checklist): implement VaultChecklist component for rendering checklist items
    Add a new VaultChecklist component that displays a list of checklist items with support for nested items. Each item includes a checkbox reflecting its checked state and links are processed to replace with vault links. This enhances the checklist functionality in the application.

---
+++ components/layout/vault/vault-checklist.tsx
@@ -0,0 +1,32 @@
+import { TChecklistItem } from '@/lib/types';
+import { replaceChecklistLinksWithVaultLinks } from '@/lib/utils';
+
+export function VaultChecklist({
+  items,
+  spacing: nestedSpacing,
+}: {
+  items: TChecklistItem[];
+  spacing?: string;
+}) {
+  return (
+    <ul className={`list-none space-y-2 pl-5 text-xl ${nestedSpacing || ''}`}>
+      {items.map((item, idx) => (
+        <li key={idx} className="task-list-item">
+          <label className="flex cursor-not-allowed items-start gap-2">
+            <input
+              type="checkbox"
+              checked={item.meta?.checked ?? false}
+              readOnly
+              className="peer hidden"
+            />
+            <span className="peer-checked:bg-primary peer-checked:border-checkmark bg-background border-checkmark peer-checked:before:text-checkmark grid h-6 w-6 place-items-center rounded border before:content-[''] peer-checked:before:text-sm peer-checked:before:content-['✔']" />
+            <span>{replaceChecklistLinksWithVaultLinks(item.content)}</span>
+          </label>
+          {item.items && item.items.length > 0 && (
+            <VaultChecklist items={item.items} spacing="mt-1" />
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+}
