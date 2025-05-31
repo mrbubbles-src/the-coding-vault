faa3c70bc0b90878932faf39e7c23bbb588adfaa
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-05-31 21:42:57 +0200

    feat(vault-page): add initial VaultPage component with placeholder content

---
+++ app/(vault)/vault/page.tsx
@@ -0,0 +1,12 @@
+const VaultPage = () => {
+  return (
+    <div>
+      <h1>
+        This is currently a Placeholder Page. Please check back later and for
+        the time being, click on the categories in the sidebar on your left!
+      </h1>
+    </div>
+  );
+};
+
+export default VaultPage;
