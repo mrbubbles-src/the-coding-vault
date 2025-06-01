3648f8a7d4de7c023a9ac4eecd7f0d280801270e
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-06-01 21:52:21 +0200

    refactor: simplify VaultAuthorAvatar component by removing unnecessary wrapper and button

--- components/layout/vault/vault-author/vault-author-avatar.tsx
+++ components/layout/vault/vault-author/vault-author-avatar.tsx
@@ -1,5 +1,3 @@
-import { PlusCircleIcon } from 'lucide-react';
-
 import {
   Avatar,
   AvatarFallback,
@@ -14,20 +12,14 @@ const VaultAuthorAvatar = ({
   authorName: string;
 }) => {
   return (
-    <div className="relative w-fit">
-      <Avatar className="size-20">
-        <AvatarImage
-          src={avatarSrc}
-          alt={authorName}
-          className="object-cover object-center"
-        />
-        <AvatarFallback className="text-xs">{authorName}</AvatarFallback>
-      </Avatar>
-      <button className="focus-visible:ring-ring/50 absolute -end-1 -bottom-1 inline-flex cursor-pointer items-center justify-center rounded-full focus-visible:ring-[3px] focus-visible:outline-none">
-        <PlusCircleIcon className="text-background size-5 fill-slate-400" />
-        <span className="sr-only">Add</span>
-      </button>
-    </div>
+    <Avatar className="size-20">
+      <AvatarImage
+        src={avatarSrc}
+        alt={authorName}
+        className="object-cover object-center"
+      />
+      <AvatarFallback className="text-xs">{authorName}</AvatarFallback>
+    </Avatar>
   );
 };
 
