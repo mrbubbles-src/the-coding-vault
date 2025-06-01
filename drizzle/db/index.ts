e2d84ab7b239b8bd4a2414be13c7ca5dcdfe1e31
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-06-02 01:37:53 +0200

    refactor: restructure database initialization and global db interface
    - Moved the database client initialization into a separate variable for clarity.
    - Declared a global interface for db to enhance type safety.
    - Ensured the db is only assigned to globalThis in non-production environments.

--- drizzle/db/index.ts
+++ drizzle/db/index.ts
@@ -3,10 +3,35 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import * as relations from './relations';
 
-const client = postgres(process.env.DATABASE_URL!);
-export const db = drizzle(client, {
+declare global {
+  interface GlobalThis {
+    db?: ReturnType<typeof drizzle>;
+  }
+}
+
+const queryClient = postgres(process.env.DATABASE_URL!, {
+  max: 5,
+});
+
+export const db = drizzle(queryClient, {
   schema: {
     ...schema,
     ...relations,
   },
 });
+
+if (process.env.NODE_ENV !== 'production') (globalThis as GlobalThis).db = db;
+
+// DO NOT DELETE THIS FOR THE TIME BEING!
+// import { drizzle } from 'drizzle-orm/postgres-js';
+// import postgres from 'postgres';
+// import * as schema from './schema';
+// import * as relations from './relations';
+
+// const client = postgres(process.env.DATABASE_URL!);
+// export const db = drizzle(client, {
+//   schema: {
+//     ...schema,
+//     ...relations,
+//   },
+// });
