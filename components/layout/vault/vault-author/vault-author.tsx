4f6a30c14f97353ca47d30cc5572b580f83cb5b7
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-06-02 02:26:47 +0200

    feat: enhance VaultAuthor component to display author dates with localization
    - Added 'dates' prop to VaultAuthor component to handle createdAt and updatedAt fields.
    - Implemented localized date formatting for both created and updated timestamps.
    - Improved author display logic for both string and object types.

--- components/layout/vault/vault-author/vault-author.tsx
+++ components/layout/vault/vault-author/vault-author.tsx
@@ -23,29 +23,95 @@ interface VaultAuthorProps {
         };
       }
     | string;
+  dates: {
+    createdAt: Date;
+    updatedAt: Date;
+  };
 }
-const VaultAuthor = ({ author }: VaultAuthorProps) => {
+const VaultAuthor = ({ author, dates }: VaultAuthorProps) => {
   const [isOpen, setIsOpen] = useState(false);
-  if (typeof author === 'string') return <span>Author: {author}</span>;
+  if (typeof author === 'string')
+    return (
+      <section>
+        <p className="text-md italic">
+          <span className="font-semibold">Autor: </span>
+          <span className="text-primary font-bold">{author}</span>
+        </p>
+        <p className="text-md italic">
+          <span className="font-semibold">Erstellt: </span>
+          <span
+            className={`${dates.createdAt.getTime() !== dates.updatedAt.getTime() ? 'text-accent' : 'text-secondary'} font-bold`}>
+            {new Date(dates.createdAt).toLocaleDateString('de-DE', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+            })}{' '}
+            Uhr
+          </span>
+        </p>
+        {dates.createdAt.getTime() !== dates.updatedAt.getTime() && (
+          <p className="text-md italic">
+            <span className="font-semibold">Aktualisiert: </span>
+            <span className="text-secondary font-bold">
+              {new Date(dates.updatedAt).toLocaleDateString('de-DE', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+              })}{' '}
+              Uhr
+            </span>
+          </p>
+        )}
+      </section>
+    );
 
   return (
     <section className="group relative inline-block w-full">
-      <p className="text-md">
-        <span>Author: </span>
+      <p className="text-md italic">
+        <span className="font-semibold">Autor: </span>
         <span
           onClick={() => setIsOpen((prev) => !prev)}
-          className={`cursor-pointer transition-all duration-300 ease-in-out ${
-            isOpen ? 'text-primary font-bold' : ''
-          } lg:group-hover:text-primary/80 lg:group-hover:font-bold`}>
+          className={`text-primary lg:group-hover:text-primary/80 cursor-pointer font-bold transition-all duration-300 ease-in-out ${isOpen ? 'text-primary/80' : ''}`}>
           {author.name}
         </span>
       </p>
-
-      <div className="pointer-events-none absolute top-full left-0 z-[1000] hidden opacity-0 transition-opacity duration-200 ease-in-out group-hover:pointer-events-auto group-hover:opacity-100 lg:block lg:group-hover:block">
+      <p className="text-md italic">
+        <span className="font-semibold">Erstellt: </span>
+        <span
+          className={`${dates.createdAt.getTime() !== dates.updatedAt.getTime() ? 'text-accent' : 'text-secondary'} font-bold`}>
+          {new Date(dates.createdAt).toLocaleDateString('de-DE', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+          })}{' '}
+          Uhr
+        </span>
+      </p>
+      {dates.createdAt.getTime() !== dates.updatedAt.getTime() && (
+        <p className="text-md italic">
+          <span className="font-semibold">Aktualisiert: </span>
+          <span className="text-secondary font-bold">
+            {new Date(dates.updatedAt).toLocaleDateString('de-DE', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+            })}{' '}
+            Uhr
+          </span>
+        </p>
+      )}
+      <div className="pointer-events-none absolute top-full left-0 z-[1000] hidden opacity-0 transition-opacity duration-200 ease-in-out group-hover:pointer-events-auto group-hover:opacity-100 lg:block">
         <VaultAuthorCard author={author} />
       </div>
 
-      {/* Mobile Card unterhalb des gesamten Satzes */}
       {isOpen && (
         <div className="absolute top-full left-0 z-[1000] lg:hidden">
           <VaultAuthorCard author={author} />
