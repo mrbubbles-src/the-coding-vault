db7979b0d079b713df0ceceeca9d38de67db1446
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-06-06 21:04:12 +0200

    feat: integrate Tooltip functionality into VaultSidebarEntryLink for enhanced user interaction

--- components/layout/vault/sidebar/vault-sidebar-entry-link.tsx
+++ components/layout/vault/sidebar/vault-sidebar-entry-link.tsx
@@ -4,6 +4,11 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { SidebarMenuButton } from '@/components/ui/shadcn/sidebar';
 import { Route } from 'next';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/shadcn/tooltip';
 
 interface VaultSidebarEntryLinkProps {
   slug: string;
@@ -15,15 +20,24 @@ const VaultSidebarEntryLink = ({ slug, title }: VaultSidebarEntryLinkProps) => {
   const isActive = pathname.toLowerCase() === `/vault/${slug}`.toLowerCase();
 
   return (
-    <SidebarMenuButton
-      variant={'outline'}
-      className={`opacity-0 transition-all duration-300 ease-in-out group-data-[state=open]/collapsible:opacity-100 ${isActive ? 'scale-105' : ''}`}
-      asChild
-      isActive={isActive}>
-      <Link href={`/vault/${slug}` as Route} prefetch={false}>
-        <span>{title}</span>
-      </Link>
-    </SidebarMenuButton>
+    <Tooltip delayDuration={300}>
+      <TooltipTrigger>
+        <SidebarMenuButton
+          variant={'outline'}
+          className={`opacity-0 transition-all duration-300 ease-in-out group-data-[state=open]/collapsible:opacity-100 ${isActive ? 'scale-105' : ''}`}
+          asChild
+          isActive={isActive}>
+          <Link href={`/vault/${slug}` as Route} prefetch={false}>
+            <span>{title}</span>
+          </Link>
+        </SidebarMenuButton>
+      </TooltipTrigger>
+      <TooltipContent
+        side="right"
+        className="TooltipContent z-[1001] max-w-[20rem] font-bold text-pretty md:max-w-full">
+        {title}
+      </TooltipContent>
+    </Tooltip>
   );
 };
 
