577268495f602bbc68d0b2ff1bc7d5a611163d85
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-07-13 16:50:04 +0200

    refactor: streamline login route by adding current user check and improving error handling

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -5,44 +5,52 @@ import { createJWT } from '@/lib/auth';
 import { createCookie } from '@/lib/cookies';
 
 import bcrypt from 'bcryptjs';
+import { sql } from 'drizzle-orm';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  const body = await req.json();
-  const { username, password } = body;
-  const user = await db.query.users.findFirst({
-    where: (users, { eq }) => eq(users.username, username),
-  });
-
-  if (!user)
-    return NextResponse.json(
-      { msg: 'Benutzername konnte nicht gefunden werden.' },
-      { status: 401, statusText: 'Unauthorized' },
-    );
-
-  const validatePassword = await bcrypt.compare(password, user.password);
-
-  if (!validatePassword)
-    return NextResponse.json(
-      { msg: 'Das eingegebene Passwort ist nicht korrekt.' },
-      { status: 401, statusText: 'Unauthorized' },
-    );
-
   try {
+    const whoami = await db.execute(sql`SELECT current_user`);
+    console.log('[CURRENT USER]', whoami);
+    const body = await req.json();
+    console.log('[LOGIN BODY]', body);
+    const { username, password } = body;
+
+    const user = await db.query.users.findFirst({
+      where: (users, { eq }) => eq(users.username, username),
+    });
+
+    if (!user) {
+      console.log('[LOGIN ERROR] Benutzer nicht gefunden');
+      return NextResponse.json(
+        { msg: 'Benutzername konnte nicht gefunden werden.' },
+        { status: 401 },
+      );
+    }
+
+    const validatePassword = await bcrypt.compare(password, user.password);
+    if (!validatePassword) {
+      console.log('[LOGIN ERROR] Passwort ungültig');
+      return NextResponse.json(
+        { msg: 'Das eingegebene Passwort ist nicht korrekt.' },
+        { status: 401 },
+      );
+    }
+
     const token = await createJWT({
       id: user.id,
       username: user.username,
       role: user.role,
     });
     await createCookie(token);
+
+    console.log('[LOGIN SUCCESS] Token erstellt und Cookie gesetzt');
     return NextResponse.redirect(new URL('/admin/dashboard', req.url));
   } catch (error) {
+    console.error('[LOGIN CATCH]', error);
     return NextResponse.json(
-      {
-        msg: 'Login fehlgeschlagen!',
-        error,
-      },
-      { status: 401, statusText: 'Unauthorized' },
+      { msg: 'Login fehlgeschlagen!', error: String(error) },
+      { status: 500 },
     );
   }
 }
