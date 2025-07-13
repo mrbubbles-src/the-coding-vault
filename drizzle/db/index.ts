273e31238cd9ee35067f163c4a823745fb117bd8
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-07-13 19:08:32 +0200

    refactor: simplify database connection setup by removing unused code and imports

--- drizzle/db/index.ts
+++ drizzle/db/index.ts
@@ -1,36 +1,45 @@
-import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-import * as relations from './relations';
 
-declare global {
-  interface GlobalThis {
-    db?: ReturnType<typeof drizzle>;
-  }
-}
+const connectionString = process.env.DATABASE_URL!;
 
-const queryClient = postgres(process.env.DATABASE_URL!, {
-  max: 5,
-});
+const client = postgres(connectionString, { prepare: false });
+export const db = drizzle(client, { schema: { ...schema } });
 
-const drizzleDb = drizzle(queryClient, {
-  schema: {
-    ...schema,
-    ...relations,
-  },
-});
+// import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+// import postgres from 'postgres';
+// import * as schema from './schema';
+// import * as relations from './relations';
 
-const globalDb = (globalThis as GlobalThis).db ?? drizzleDb;
-export const db = {
-  ...globalDb,
-  $client: queryClient,
-} as PostgresJsDatabase<typeof schema & typeof relations> & {
-  $client: typeof queryClient;
-};
+// declare global {
+//   interface GlobalThis {
+//     db?: ReturnType<typeof drizzle>;
+//   }
+// }
 
-if (process.env.NODE_ENV !== 'production') {
-  (globalThis as GlobalThis).db = db;
-}
+// const queryClient = postgres(process.env.DATABASE_URL!, {
+//   max: 5,
+// });
+
+// const drizzleDb = drizzle(queryClient, {
+//   schema: {
+//     ...schema,
+//     ...relations,
+//   },
+// });
+
+// const globalDb = (globalThis as GlobalThis).db ?? drizzleDb;
+// export const db = {
+//   ...globalDb,
+//   $client: queryClient,
+// } as PostgresJsDatabase<typeof schema & typeof relations> & {
+//   $client: typeof queryClient;
+// };
+
+// if (process.env.NODE_ENV !== 'production') {
+//   (globalThis as GlobalThis).db = db;
+// }
 
 // DO NOT DELETE THIS FOR THE TIME BEING!
 // import { drizzle } from 'drizzle-orm/postgres-js';
