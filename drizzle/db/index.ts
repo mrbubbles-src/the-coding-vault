9f00ddb40dcecd71a857026ad47a74a6c0003174
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-07-13 19:16:40 +0200

    refactor: enhance database export by including relations in schema

--- drizzle/db/index.ts
+++ drizzle/db/index.ts
@@ -1,11 +1,12 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
+import * as relations from './relations';
 
 const connectionString = process.env.DATABASE_URL!;
 
 const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client, { schema: { ...schema } });
+export const db = drizzle(client, { schema: { ...schema, ...relations } });
 
 // import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 // import postgres from 'postgres';
