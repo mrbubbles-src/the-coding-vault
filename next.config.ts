10ac35e2558340ed40e18168f2fe2b12c027b9be
mrbubbles-src <manuel.fahrenholz@mrbubbles-src.dev>
2025-07-19 21:37:34 +0200

    refactor: enable devtoolSegmentExplorer in experimental options

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   experimental: {
+    devtoolSegmentExplorer: true,
     typedRoutes: true,
     useCache: true,
     mdxRs: {
